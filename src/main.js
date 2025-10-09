26908dab862345f7917242a4bd4b162aa642a3a8
IniRalfi <rflipratm@gmail.com>
2025-10-09 11:26:27 +0700

    membuat header dan css native nya

--- src/main.js
+++ src/main.js
@@ -1,1 +1,10 @@
 import "./style.css";
+
+// Import CSS Bootstap
+import "bootstrap/dist/css/bootstrap.min.css";
+
+// Import CSS Native
+import "./style.css";
+
+// Import Javascript dari bootstrap
+import "bootstrap/dist/js/bootstrap.bundle.min.js";
